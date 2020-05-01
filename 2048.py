0ee377fb4772992f8bd4265ed0085f37d2998be9
1945375000 <zhengwf@litsoft.com.cn>
2020-05-01 22:58:58 +0800

    2048

--- 2048.py
+++ 2048.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import curses
+from random import randrange,choice
 
 def main(stdscr):
+    cells=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 
     def cast(str):
         stdscr.addstr(str+'\n')
@@ -19,17 +21,42 @@ def main(stdscr):
     def draw_row(row):
         cast(''.join([get_cell(cell) for cell in row])+'|')
 
-    curses.use_default_colors()
+    def draw_game():
+        global cells
+        stdscr.clear()
+        for row in cells:
+            draw_hor()
+            draw_row(row)
+        draw_hor()
 
-    draw_hor()
-    draw_row([2,3,0,7])
-    draw_hor()
-    draw_row([0,4,6,0])
-    draw_hor()
-    draw_row([343,556,676,0])
-    draw_hor()
-    draw_row([565,676,45,6])
-    draw_hor()
+    def init_game():
+        global cells
+        cells=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+    def spawn():
+        global cells
+        new_cell=4 if randrange(100)>89 else 2
+        (i,j)=choice([(i,j) for i in range(4)  for j in range(4) if cells[i][j]==0])
+        cells[i][j]=new_cell
+
+    def move_is_possible(direction):
+        def move_left_is_possible(row):
+            def change(i):
+                if row[i]==0 and row[i+1]!=0:
+                    return True
+                if row[i]!=0 and row[i+1]==row[i]:
+                    return True
+            return any(change(i) for i in range(len(row)-1))
+
+        global cells
+        return any(move_left_is_possible(row) for row in cells)
+
+    def move():
+
+
+
+
+    curses.use_default_colors()
 
     stdscr.getch()
 curses.wrapper(main)
